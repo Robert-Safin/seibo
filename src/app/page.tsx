2d45b865787fed77d16af63f5e139a7f0d629e0a
Robert Safin <robertsafin98@gmail.com>
2024-08-05 10:37:42 +0800

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="">Bobs boobies xyzg</h1>
-      <h1 className="sh2">Bobs boobies xyzg</h1>
+
     </main>
   );
 }
