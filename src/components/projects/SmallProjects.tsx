c21b75465a47e9869a471b31fc0c9d2c1bcc0b96
Robert Safin <robertsafin98@gmail.com>
2024-08-08 13:20:23 +0800

    re

--- src/components/projects/SmallProjects.tsx
+++ src/components/projects/SmallProjects.tsx
@@ -15,161 +15,7 @@ const SmallProjects = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-[6px]">
-        <div className="relative w-full h-[335px] z-0">
-          <Image
-            src={'/assets/project-cards/quantico.png'}
-            width={1000}
-            height={1000}
-            alt={'Quantico'}
-            className="absolute -z-10 w-full h-[335px] object-cover"
-          />
-          <motion.div
-            className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
-            style={{ backgroundColor: '#CEB267' }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <div className="flex h-full justify-center items-center">
-              <div className="flex w-[154px] h-[96px] relative overflow-hidden">
-                <iframe
-                  loading="eager"
-                  src={'https://quanticotc.com/'}
-                  className="object-cover w-[1520px] h-[960px] absolute origin-top-left scale-[10%]"
-                />
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="z-0 py-[18px] px-[12px]">
-            <div className="flex w-full justify-between">
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'2024'}
-              </p>
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'Quantico'}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative w-full h-[335px] z-0">
-          <Image
-            src={'/assets/project-cards/elysium.png'}
-            width={1000}
-            height={1000}
-            alt={'Elysium'}
-            className="absolute -z-10 w-full h-[335px] object-cover"
-          />
-          <motion.div
-            className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
-            style={{ backgroundColor: '#4A0915' }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <div className="flex h-full justify-center items-center">
-              <div className="flex w-[154px] h-[96px] relative overflow-hidden">
-                <iframe
-                  loading="eager"
-                  src={'https://elysium-beta.vercel.app/'}
-                  className="object-cover w-[1520px] h-[960px] absolute origin-top-left scale-[10%]"
-                />
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="z-0 py-[18px] px-[12px]">
-            <div className="flex w-full justify-between">
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'2023'}
-              </p>
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'Elysium'}
-              </p>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="relative w-full h-[335px] z-0">
-          <Image
-            src={'/assets/project-cards/symbobtic.png'}
-            width={1000}
-            height={1000}
-            alt={'Symbobtic'}
-            className="absolute -z-10 w-full h-[335px] object-cover"
-          />
-          <motion.div
-            className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
-            style={{ backgroundColor: '#000000' }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <div className="flex h-full justify-center items-center">
-              <div className="flex w-[154px] h-[96px] relative overflow-hidden">
-                <iframe
-                  loading="eager"
-                  src={'https://symbobtic.vercel.app/'}
-                  className="object-cover w-[1520px] h-[960px] absolute origin-top-left scale-[10%]"
-                />
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="z-0 py-[18px] px-[12px]">
-            <div className="flex w-full justify-between">
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'2023'}
-              </p>
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'Symbobtic'}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative w-full h-[335px] z-0">
-          <Image
-            src={'/assets/project-cards/gaes.png'}
-            width={1000}
-            height={1000}
-            alt={'GAES'}
-            className="absolute -z-10 w-full h-[335px] object-cover"
-          />
-          <motion.div
-            className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
-            style={{ backgroundColor: '#989898' }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <div className="flex h-full justify-center items-center">
-              <div className="flex w-[154px] h-[96px] relative overflow-hidden">
-                <iframe
-                  loading="eager"
-                  src={'https://gaes.ae/'}
-                  className="object-cover w-[1520px] h-[960px] absolute origin-top-left scale-[10%]"
-                />
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="z-0 py-[18px] px-[12px]">
-            <div className="flex w-full justify-between">
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'2023'}
-              </p>
-              <p className="IT-Medium-S-M text-theme-white uppercase">
-                {'GAES'}
-              </p>
-            </div>
-          </div>
-
-        </div>
-
-        {/* <Card
+        <Card
           imagePath="/assets/project-cards/quantico.png"
           year="2024"
           title="Quantico"
@@ -196,7 +42,7 @@ const SmallProjects = () => {
           title="GAES"
           bgHex="#989898"
           iframeSrc="https://gaes.ae/"
-        /> */}
+        />
       </div>
       <div className="flex justify-between pt-[36px]">
         <p className="text-theme-gray sh2-medium">© 2023-2024</p>
@@ -216,44 +62,44 @@ type Props = {
   iframeSrc: string;
 };
 
-// const Card: FC<Props> = (props) => {
-//   return (
-//     <div className="relative w-full h-[335px] z-0">
-//       <Image
-//         src={props.imagePath}
-//         width={1000}
-//         height={1000}
-//         alt={props.title}
-//         className="absolute -z-10 w-full h-[335px] object-cover"
-//       />
-//       <motion.div
-//         className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
-//         style={{ backgroundColor: props.bgHex }}
-//         initial={{ opacity: 0 }}
-//         whileInView={{ opacity: 1 }}
-//         transition={{ duration: 3 }}
-//       >
-//         <div className="flex h-full justify-center items-center">
-//           <div className="flex w-[154px] h-[96px] relative overflow-hidden">
-//             <iframe
-//               loading="eager"
-//               src={props.iframeSrc}
-//               className="object-cover w-[1520px] h-[960px] absolute origin-top-left scale-[10%]"
-//             />
-//           </div>
-//         </div>
-//       </motion.div>
+const Card: FC<Props> = (props) => {
+  return (
+    <div className="relative w-full h-[335px] z-0">
+      <Image
+        src={props.imagePath}
+        width={1000}
+        height={1000}
+        alt={props.title}
+        className="absolute -z-10 w-full h-[335px] object-cover"
+      />
+      <motion.div
+        className={`absolute z-10 w-[191px] h-[191px] m-auto left-0 right-0 top-0 bottom-0`}
+        style={{ backgroundColor: props.bgHex }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 3 }}
+      >
+        <div className="flex h-full justify-center items-center">
+          <div className="flex w-[154px] h-[96px] relative overflow-hidden">
+            <iframe
+              loading="eager"
+              src={props.iframeSrc}
+              className="object-cover w-[720px] h-[560px] absolute origin-top-left scale-[20%]"
+            />
+          </div>
+        </div>
+      </motion.div>
 
-//       <div className="z-0 py-[18px] px-[12px]">
-//         <div className="flex w-full justify-between">
-//           <p className="IT-Medium-S-M text-theme-white uppercase">
-//             {props.year}
-//           </p>
-//           <p className="IT-Medium-S-M text-theme-white uppercase">
-//             {props.title}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="z-0 py-[18px] px-[12px]">
+        <div className="flex w-full justify-between">
+          <p className="IT-Medium-S-M text-theme-white uppercase">
+            {props.year}
+          </p>
+          <p className="IT-Medium-S-M text-theme-white uppercase">
+            {props.title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
